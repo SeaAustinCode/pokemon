d396e60e90c983ee13af298ad3aea885b17c0bbd
SeaAustinCode <seaaustincode@gmail.com>
2022-05-11 16:49:28 -0700

    pokemon-axios

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 
 function App() {
 
   const [pokemon, setPokemon] = useState([]);
 
-  const fetchPokemon = () => {
-    console.log("clicked")
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=20&limit=807")
-      .then(response => {
-        return response.json();
-      }).then(response => {
-        console.log(response.results);
-        setPokemon(response.results);
-      }).catch(err => {
-        console.log(err);
-      });
+  // const fetchPokemon = () => {
+  //   console.log("clicked")
+  //   fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+  //     .then(response => {
+  //       return response.json();
+  //     }).then(response => {
+  //       console.log(response.results);
+  //       setPokemon(response.results);
+  //     }).catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+
+  // axios 
+  const axiosFetchPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+      .then((response) => {
+        console.log(response.data.results);
+        setPokemon(response.data.results);
+      })
+      .catch(err => console.log(err))
   }
 
   return (
     <div className="App">
       <h1>Pokemon Api</h1>
       <h4> fetch / Axios / useEffect()</h4>
-      <button onClick={fetchPokemon}>Get Pokemon</button>
+      <button onClick={axiosFetchPokemon}>Axios Get Pokemon</button>
+      {
+        pokemon.map((pokemon, index) => {
+          return <li key={index}>{pokemon.name}</li>
+        })
+      }
+      {/* <button onClick={fetchPokemon}>Get Pokemon</button>
       {
         pokemon.map((pokemon, index) => {
           return <p key={index}>{pokemon.id}{pokemon.name}</p>
         })
-      }
+      } */}
     </div>
-  )};
+  );
 
+}
 
-
-  export default App;
+export default App;
