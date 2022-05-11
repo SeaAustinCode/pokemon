a691a73a55eac1430e0c2d5ca7cb9decba8179fa
SeaAustinCode <seaaustincode@gmail.com>
2022-05-11 14:41:24 -0700

    pokemon-api-get-807-pokemon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchPokemon = () => {
+    console.log("clicked")
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=20&limit=807")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+        console.log(response.results);
+        setPokemon(response.results);
+      }).catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon Api</h1>
+      <h4> fetch / Axios / useEffect()</h4>
+      <button onClick={fetchPokemon}>Get Pokemon</button>
+      {
+        pokemon.map((pokemon, index) => {
+          return <p key={index}>{pokemon.id}{pokemon.name}</p>
+        })
+      }
     </div>
-  );
-}
+  )};
 
-export default App;
+
+
+  export default App;
